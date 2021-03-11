4748abb58a4bb8b9f5b0cc5fd991aa8291a4ba04
John Madden <jck13mad@gmail.com>
2021-03-10 18:12:13 -0600

    Done

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-
+var num1 = 2;
+var num2 = 31;
 // Write your code here
+var multiply = num1 * num2;
+var random = Math.floor(Math.random() * 10) + 1;
+var mod = 19%5;
+var max = Math.max(1, 2, 5);
