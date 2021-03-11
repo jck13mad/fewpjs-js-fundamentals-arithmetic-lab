5394a1f3a4e67c61a616e94830c87e139c9bf037
John Madden <jck13mad@gmail.com>
2021-03-10 18:14:50 -0600

    commit tree 38e44211bcf6c26c9d79e8ea4ba11884c3d83a44

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var num1 = 2;
 var num2 = 31;
+var num3 = 19;
+var num4 = 5;
 // Write your code here
 var multiply = num1 * num2;
 var random = Math.floor(Math.random() * 10) + 1;
-var mod = 19%5;
-var max = Math.max(1, 2, 5);
+var mod = num3%num4;
+var max = Math.max(1, 2, 20);
